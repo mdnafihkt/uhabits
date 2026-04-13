564f2532ab197f9be8b1224fcf030faefe810f0b
Mohamed Nafih <102818533+mdnafihkt@users.noreply.github.com>
2026-04-13 16:04:45 +0530

    chore: update preview image

--- uhabits-android/build.gradle.kts
+++ uhabits-android/build.gradle.kts
@@ -44,8 +44,8 @@ android {
     compileSdk = 36
 
     defaultConfig {
-        versionCode = 20302
-        versionName = "2.4.0"
+        versionCode = 20303
+        versionName = "2.4.1"
         minSdk = 28
         targetSdk = 36
         applicationId = "com.daimens.uhabits"
